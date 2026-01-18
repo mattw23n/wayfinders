d6bd4f6d59ca62bbc2c499182697488445864934
SQ77 <andrewzsq@gmail.com>
2026-01-18 10:06:39 +0800

    Fix frontend cors url

--- backend/main.py
+++ backend/main.py
@@ -67,7 +67,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://wayfinders-hnr-2026.vercel.app", "https://wayfinders-six.vercel.app/"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://wayfinders-hnr-2026.vercel.app", "https://wayfinders-six.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
